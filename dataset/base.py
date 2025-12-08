09ab3410627c39ceea20516dccebdaa04161997c
Bangchis <tienqldtdata@gmail.com>
2025-12-08 14:20:42 +0700

    adsfas

--- dataset/base.py
+++ dataset/base.py
@@ -18,7 +18,8 @@ class BaseSetsDataset(data.Dataset):
         num_classes_task: for generative models we use 1 concept per set.
         split: train/val/test
         augment: augment the sets with flipping and/or rotations (only for binary datasets).
-    """ 
+    """
+
     def __init__(self,
                  dataset,
                  data_dir,
@@ -32,12 +33,12 @@ class BaseSetsDataset(data.Dataset):
 
         self.dts = {"omniglot_back_eval": {"size": 28, "img_cls": 20, "nc": 1, "tr": 964, "vl": 97, "ts": 659},
                     "omniglot_random": {"size": 28, "img_cls": 20, "nc": 1, "tr": 964, "vl": 97, "ts": 659},
-                    "doublemnist":  {"size": 28, "img_cls": 1000, "nc": 1, "tr":64, "vl": 16, "ts": 20},
+                    "doublemnist":  {"size": 28, "img_cls": 1000, "nc": 1, "tr": 64, "vl": 16, "ts": 20},
                     "triplemnist":  {"size": 28, "img_cls": 1000, "nc": 1, "tr": 640, "vl": 160, "ts": 200},
-                    "minimagenet":  {"size": 32, "img_cls": 600, "nc": 3, "tr": 64, "vl": 16 , "ts": 20}, 
-                                     #"shift": -112.6077, "scale": 1. / 68.315056},
-                    "cifar100":     {"size": 32, "img_cls": 600, "nc": 3, "tr":60, "vl":20, "ts": 20},
-                    "cifar100mix":     {"size": 32, "img_cls": 600, "nc": 3, "tr":60, "vl":20, "ts": 20},
+                    "minimagenet":  {"size": 32, "img_cls": 600, "nc": 3, "tr": 64, "vl": 16, "ts": 20},
+                    # "shift": -112.6077, "scale": 1. / 68.315056},
+                    "cifar100":     {"size": 32, "img_cls": 600, "nc": 3, "tr": 60, "vl": 20, "ts": 20},
+                    "cifar100mix":     {"size": 32, "img_cls": 600, "nc": 3, "tr": 60, "vl": 20, "ts": 20},
                     "cub":          {"size": 64, "img_cls": 60, "nc": 3, "tr": 100, "val": 50, "ts": 50},
                     }
         self.data_dir = data_dir
@@ -46,8 +47,8 @@ class BaseSetsDataset(data.Dataset):
         self.augment = augment
         self.binarize = binarize
         self.sample_size = sample_size + 1
-        self.norm=norm
-        
+        self.norm = norm
+
         self.nc = self.dts[dataset]["nc"]
         self.size = self.dts[dataset]["size"]
         self.img_cls = self.dts[dataset]["img_cls"]
@@ -55,19 +56,20 @@ class BaseSetsDataset(data.Dataset):
 
         self.images, self.labels, self.map_cls = self.get_data()
         self.split_train_val()
-        
+
         print(self.split)
         print(self.images.shape, self.labels.shape)
 
         self.init_sets()
-        
+
     def init_sets(self):
         sets, set_labels = self.make_sets(self.images, self.labels)
         if self.split in ["train", "train_indistro"]:
             if self.augment:
                 sets, set_labels = self.augment_sets(sets, set_labels)
 
-        sets = sets.reshape(-1, self.sample_size, self.nc, self.size, self.size)
+        sets = sets.reshape(-1, self.sample_size,
+                            self.nc, self.size, self.size)
         self.n = len(sets)
         self.data = {
             'inputs': sets,
@@ -76,8 +78,9 @@ class BaseSetsDataset(data.Dataset):
 
     def get_data(self):
         img = []
-        
-        path = os.path.join(self.data_dir, self.dataset, self.split + "_" + self.dataset + ".pkl")
+
+        path = os.path.join(self.data_dir, self.dataset,
+                            self.split + "_" + self.dataset + ".pkl")
         with open(path, 'rb') as f:
             file = pickle.load(f)
 
@@ -86,7 +89,7 @@ class BaseSetsDataset(data.Dataset):
 
             map_cls[i] = k
             value = file[k]
-            
+
             # if only one channel (1, img_dim, img_dim)
             if self.dataset in ["doublemnist", "triplemnist"]:
                 value = np.expand_dims(value, axis=1)
@@ -111,7 +114,8 @@ class BaseSetsDataset(data.Dataset):
     def __getitem__(self, item, lbl=None):
         samples = self.data['inputs'][item]
         # all datasets should be in [0, 1]
-        if self.dataset in ['minimagenet', 'cub', 'cifar100', "celeba", "omniglot_back_eval",  "cifar100mix"]: #and self.norm:
+        # and self.norm:
+        if self.dataset in ['minimagenet', 'cub', 'cifar100', "celeba", "omniglot_back_eval",  "cifar100mix"]:
 
             if self.dataset == "omniglot_back_eval":
                 # dequantize
@@ -120,7 +124,7 @@ class BaseSetsDataset(data.Dataset):
                 samples = samples + (np.random.random(samples.shape) - 0.5)
                 samples = samples / 255.
                 samples = samples.astype(np.float32)
-                
+
             # rescale to [-1, 1]
             samples = 2 * samples - 1
         if lbl:
@@ -152,8 +156,8 @@ class BaseSetsDataset(data.Dataset):
                 augmented[s] = augmented[s, :, :, :, ::-1]
             if flip_vertical:
                 augmented[s] = augmented[s, :, :, ::-1, :]
-        
-        # if self.dataset in ["doublemnist", "triplemnist"]:      
+
+        # if self.dataset in ["doublemnist", "triplemnist"]:
         #     #rotate images in set only if binary
         #     for s in range(n_sets):
         #         angle = np.random.uniform(-10, 10)
@@ -162,7 +166,7 @@ class BaseSetsDataset(data.Dataset):
 
             # even if the starting images are binarized, the augmented one are not
             # augmented = np.random.binomial(1, p=augmented, size=augmented.shape).astype(np.float32)
-            
+
         augmented = np.concatenate([augmented, sets])
         augmented_lbl = np.concatenate([augmented_lbl, sets_lbl])
 
@@ -209,8 +213,9 @@ class BaseSetsDataset(data.Dataset):
 
             # Reshape into sets
             class_sets = truncated_images.reshape(n_sets, self.sample_size,
-                                                 self.nc, self.size, self.size)
-            class_label_sets = truncated_labels.reshape(n_sets, self.sample_size)
+                                                  self.nc, self.size, self.size)
+            class_label_sets = truncated_labels.reshape(
+                n_sets, self.sample_size)
 
             all_sets.append(class_sets)
             all_labels.append(class_label_sets)
@@ -229,11 +234,11 @@ class BaseSetsDataset(data.Dataset):
     def split_train_val(self, ratio=0.9):
         if self.dataset in ["omniglot_back_eval"]:
 
-            s = int(ratio* self.images.shape[0])
+            s = int(ratio * self.images.shape[0])
             if self.split == "train":
-                self.images = self.images #[:s]
-                self.labels = self.labels #[:s]
-            
+                self.images = self.images  # [:s]
+                self.labels = self.labels  # [:s]
+
             elif self.split == "train_indistro":
                 self.images = self.images[:s][:, :15]
                 self.labels = self.labels[:s][:, :15]
@@ -250,7 +255,8 @@ class BaseSetsDataset(data.Dataset):
 
 if __name__ == "__main__":
 
-    dataset = BaseSetsDataset(dataset="cub", data_dir="/home/gigi/ns_data/", sample_size=5, split="val", augment=False)
+    dataset = BaseSetsDataset(
+        dataset="cub", data_dir="/home/gigi/ns_data/", sample_size=5, split="val", augment=False)
     print(dataset.data["inputs"].shape)
     print(dataset.data["targets"].shape)
     print(len(dataset))
